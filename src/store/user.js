896519267cefc809986ffa526ca2f5c2926d10cd
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-03 17:03:44 -0300

    feat: create userLogout function

--- src/store/user.js
+++ src/store/user.js
@@ -1,16 +1,24 @@
 import { USER_GET } from '../api';
 import createAsyncSlice from './helper/createAsyncSlice';
-import { fetchToken } from './token';
+import { fetchToken, resetTokenState } from './token';
 
 const slice = createAsyncSlice({
   name: 'user',
   fetchConfig: (token) => USER_GET(token),
 });
 
+const { resetState: resetUserState } = slice.actions;
+
 export const fetchUser = slice.asyncAction;
 export const userLogin = (user) => async (dispatch) => {
   const { payload } = await dispatch(fetchToken(user));
   if (payload.token) await dispatch(fetchUser(payload.token));
+};
+
+export const userLogout = () => async (dispatch) => {
+  dispatch(resetUserState());
+  dispatch(resetTokenState());
+  window.localStorage.removeItem('token');
 };
 
 export default slice.reducer;
