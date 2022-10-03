6b23f2b25a58ababb0972cef61f94634f423e746
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-03 15:56:24 -0300

    feat: verify api error

--- src/store/helper/createAsyncSlice.js
+++ src/store/helper/createAsyncSlice.js
@@ -46,6 +46,7 @@ const createAsyncSlice = (config) => {
       const { url, options } = config.fetchConfig(payload);
       const response = await fetch(url, options);
       const data = await response.json();
+      if (response.ok === false) throw new Error(data.message);
       return dispatch(fetchSuccess(data));
     } catch (error) {
       return dispatch(fetchError(error.message));
