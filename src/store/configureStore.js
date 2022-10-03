f143e7d82db22a88e0aa3cb0aa2e4e90439dff16
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-03 14:37:04 -0300

    test: create example reducer  to test

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-const reducer = combineReducers({});
+const contador = () => 0;
+
+const reducer = combineReducers({ contador });
 
 const store = configureStore({
   reducer,
