a74d9e5c2716a4d637d524e305eeb038c340bc30
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-03 16:47:05 -0300

    refactor: get data and dispatch from state

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -5,20 +5,27 @@ import { Link } from 'react-router-dom';
 import Input from '../Form/Input';
 import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
-import { UserContext } from '../../UserContext';
 import Error from '../Helper/Error';
 import Head from '../Helper/Head';
+import { useDispatch, useSelector } from 'react-redux';
+import { userLogin } from '../../store/user';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-  const { userLogin, error, loading } = React.useContext(UserContext);
+
+  const dispatch = useDispatch();
+  const { token, user } = useSelector((state) => state);
+  const loading = token.loading || user.loading;
+  const error = token.error || user.error;
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      userLogin(username.value, password.value);
+      dispatch(
+        userLogin({ username: username.value, password: password.value }),
+      );
     }
   }
 
