69726e7fd6b9cf05539e762428e2cb36a2d60b16
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-03 16:49:32 -0300

    refactor: remove UserContext

--- src/App.js
+++ src/App.js
@@ -9,33 +9,30 @@ import Photo from './Components/Photo/Photo';
 import UserAccount from './Components/User/UserAccount';
 import UserProfile from './Components/User/UserProfile';
 import NotFound from './Components/NotFound';
-import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <UserStorage>
-          <Header />
-          <main className="AppBody">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="login/*" element={<Login />} />
-              <Route
-                path="user/*"
-                element={
-                  <ProtectedRoute>
-                    <UserAccount />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="photo/:id" element={<Photo />} />
-              <Route path="profile/:user" element={<UserProfile />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </UserStorage>
+        <Header />
+        <main className="AppBody">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="user/*"
+              element={
+                <ProtectedRoute>
+                  <UserAccount />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="photo/:id" element={<Photo />} />
+            <Route path="profile/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </BrowserRouter>
     </div>
   );
